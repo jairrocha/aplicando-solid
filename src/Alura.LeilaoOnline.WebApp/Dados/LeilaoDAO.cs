eb6d60033b710482c60ab5903f337743aea2f481
Jair <jairmsn@hotmail.com>
2022-02-04 23:16:51 -0300

    Aplicando o 'Single responsibility principle' no projeto. (Retirando da controller a responsabilidade de lidar diretamente com Entity).

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/LeilaoDAO.cs
@@ -0,0 +1,48 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public class LeilaoDAO
+    {
+
+        public AppDbContext _context;
+
+        public LeilaoDAO()
+        {
+            _context = new AppDbContext();
+        }
+        public IEnumerable<Categoria> BuscaCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                .Include(l => l.Categoria)
+                .ToList();
+        }
+        public Leilao BuscaLeilaoPorId(int id)
+        {
+            return _context.Leiloes.First(l => l.Id == id);
+        }
+        public void Inserir(Leilao leilao)
+        {
+            _context.Add(leilao);
+            _context.SaveChanges();
+        }
+        public void Alterar(Leilao leilao)
+        {
+            _context.Update(leilao);
+            _context.SaveChanges();
+        }
+        public void Remover(Leilao leilao)
+        {
+            _context.Remove(leilao);
+            _context.SaveChanges();
+        }
+
+    }
+}
